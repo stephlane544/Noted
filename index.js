ba2944cd07638f4a9c657b44657c7541e7e63d62
stephlane544 <stephlane544@gmail.com>
2019-04-09 11:53:37 -0600

    deploy

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const port = process.env.PORT || 5000;
 const mongoose = require('mongoose');
 const expressJwt = require('express-jwt');
 const path = require('path');
-const secret = process.env.SECRET || "some secret passphrase here for local development"
 
 
 app.use(express.json());
@@ -21,7 +20,10 @@ app.use((err, req, res, next) => {
     return res.send({message: err.message});
 })
 
-mongoose.connect(process.env.mongolab-contoured-55016 || 'mongodb://localhost:27017/noted', {useNewUrlParser: true}).then(() => {
+// mongodb://heroku_39vqgsgh:jjti3k4e3nth6csf08qnvggcdm@ds125673.mlab.com:25673/heroku_39vqgsgh
+// mongolab-contoured-55016
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/noted', {useNewUrlParser: true}).then(() => {
     console.log('Connected to MongoDB')
 }).catch(err => console.log(err));
 
