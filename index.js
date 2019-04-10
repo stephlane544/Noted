834e133e3410ba49ffb512647e33be5db70ea82a
stephlane544 <stephlane544@gmail.com>
2019-04-10 09:43:17 -0600

    deployment

--- index.js
+++ index.js
@@ -15,10 +15,6 @@ app.use('/api', require('./routes/apiRoutes'));
 app.use('/auth', require('./routes/authRoutes'));
 
 
-
-// mongodb://heroku_39vqgsgh:jjti3k4e3nth6csf08qnvggcdm@ds125673.mlab.com:25673/heroku_39vqgsgh
-// mongolab-contoured-55016
-
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/noted', {useNewUrlParser: true}).then(() => {
     console.log('Connected to MongoDB')
 }).catch(err => console.log(err));
